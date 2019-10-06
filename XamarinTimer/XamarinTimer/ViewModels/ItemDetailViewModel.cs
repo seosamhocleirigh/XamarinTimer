a1d30d86c1de555fe3e3c74012efff0af26be7bf
seosamhocleirigh <seosamhocleirigh@gmail.com>
2019-10-06 15:57:37 +0100

    Created template in VS

---
+++ XamarinTimer/XamarinTimer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinTimer.Models;
+
+namespace XamarinTimer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
